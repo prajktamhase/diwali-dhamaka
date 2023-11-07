b40febb70ab0538cdffe5ffad948e30968d501b9
prajktamhase <mhaseprajkta@gmail.com>
2023-11-07 12:13:27 +0800

    diwali dhamaka

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Home from "./views/Home/Home"
+import { RouterProvider , createBrowserRouter } from 'react-router-dom'
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<Home/>
+  }
+])
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={router}/>);
+ 
+  
+
